9ff068a1ab4d1e76863721e89cd07afe2baecfa1
Thayscosta95 <tha_tacosta@hotmail.com>
2025-07-14 21:04:39 -0300

    ajustes na tela principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { HomeComponent } from './componentes/home/home.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TecnologiasComponent } from './componentes/tecnologias/tecnologias.component';
 
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TecnologiasComponent
+  imports: [RouterOutlet, TecnologiasComponent, HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
